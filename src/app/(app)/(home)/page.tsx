ea1e55fde2da9689f835d42ad74c9d205a3b0690
Izaias Lima <izaiaslima356@gmail.com>
2025-01-28 15:44:45 -0300

    fix: variável action types sendo usada como tipo

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { InvitesTable } from "@/components/invites/invites-table";
 import { Button } from "../../../components/ui/button";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function Home() {
-	const router = useRouter();
 
 	return (
 		<>
